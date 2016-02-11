1103c3ff976c3ad83ec22cf3d05b510f2f181af4
jlago <juliolagoneto@gmail.com>
2016-02-11 11:04:36 -0200

    m1c3 e m1c4

--- calc/src/main/java/demoisellelab/CalculatorREST.java
+++ calc/src/main/java/demoisellelab/CalculatorREST.java
@@ -1,9 +1,12 @@
 package demoisellelab;
 
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+
+import br.gov.frameworkdemoiselle.util.ValidatePayload;
 
 @Path("calculator")
 public class CalculatorREST {
@@ -12,12 +15,15 @@ public class CalculatorREST {
 	@Path("sum")
 	@Consumes("application/json")
 	@Produces("text/plain")
+	@ValidatePayload
 	public Integer sum(Data data){
 		return data.a+ data.b;
 	}
 	
 	public static class Data{
+		@NotNull
 		public Integer a;
+		@NotNull
 		public Integer b;
 	}
 }
