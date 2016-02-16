3b156bc81c984c340cc56ec949ed60aca92d5ced
jlago <juliolagoneto@gmail.com>
2016-02-16 16:45:18 -0200

    m1c5

--- calc/src/main/webapp/index.js
+++ calc/src/main/webapp/index.js
@@ -4,19 +4,31 @@ $(function() {
  
 function somar(event) {
 	event.preventDefault();
+	 $(".required").removeClass("required");
+	 $("span").empty();
 	
 	var data = {
             a : $("#a").val(),
             b : $("#b").val()
     }
     
-   Calculator.sum(data).done(sumOK);
+   Calculator.sum(data).done(sumOK).fail(sumFailed);
 };
 
 function sumOK(data){
 	$("#result").val(data);
 }
 
+function sumFailed(jqXHR) {
+    if (jqXHR.status == 422) {
+        var data = JSON.parse(jqXHR.responseText);
+ 
+        $.each(data, function(index, value) {
+            $("#" + value.property).addClass("required");
+            $("#" + value.property + "-message").text(value.message);
+        });
+    }
+}
 
 
 
